c402de0cfde88058f1130582ab6530b95fa6edab
ldef <lilian.defarges@gmail.com>
2017-08-03 11:04:48 +0200

    Translation in sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { CoreModule } from '../core/core.module';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MobileSidebarToggleDirective, SidebarToggleDirective } from './sidebar/sidebar.directive';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, CoreModule],
   declarations: [
     HeaderComponent,
     SidebarComponent,
