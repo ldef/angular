f8596d708d66d4e0c4ebd72ba4ef52b76559bc76
ldef <lilian.defarges@gmail.com>
2017-11-06 15:34:48 +0100

    Angular v5

---
+++ src/app/core/storage/storage.service.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { StorageService } from './storage.service';
+const DATA = { user: 'test' };
+const KEY = 'key';
+
+describe('Storage service tests', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [StorageService]
+        });
+    });
+
+    it('should inject service', inject([StorageService], (service: StorageService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('should update data in local storage', inject([StorageService], (service: StorageService) => {
+        service.setItem(KEY, DATA);
+        expect(service.getItem(KEY)).toEqual(DATA);
+        service.removeItem(KEY);
+        expect(service.getItem(KEY)).toBeNull();
+    }));
+
+    it('should clear storage', inject([StorageService], (service: StorageService) => {
+        service.setItem(KEY, DATA);
+        expect(service.getItem(KEY)).toEqual(DATA);
+        service.clear();
+        expect(service.getItem(KEY)).toBeNull();
+    }));
+});
